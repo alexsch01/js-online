6d5054556b91394b143669205dd2a8ba19ae5658
Alex Schwartz <alexschwartz01@gmail.com>
2024-03-17 14:20:06 -0400

    Create task.js

---
+++ task.js
@@ -0,0 +1,30 @@
+self.onmessage = (event) => {
+  (async (code) => {
+    let output = ""
+
+    console.log = function(...args) {
+      args = args.map(elem => {
+          if(elem === undefined) {
+              return 'undefined'
+          } else if(typeof elem == 'object') {
+              return JSON.stringify(elem)
+          } else {
+              return elem
+          }
+      })
+      output += args.join(" ") + "\n"
+    }
+
+    globalThis.print = function(arg) {
+      console.log(JSON.stringify(arg, null, 2))
+    }
+
+    try {
+      await eval(code)
+    } catch(err) {
+      console.log(err.toString())
+    }
+
+    postMessage({ queryMethodArguments: [output] })
+  }).apply(self, event.data.queryMethodArguments)
+}
