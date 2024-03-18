3947d152ce2c20d0b01c786808cb483f9c2673a9
Alex Schwartz <alexschwartz01@gmail.com>
2024-03-18 14:13:53 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,30 +1,30 @@
 self.onmessage = async (event) => {
-  let output = ""
-  const code = event.data.queryMethodArguments[0]
+    let output = ""
+    const code = event.data.queryMethodArguments[0]
 
-  console.log = function(...args) {
-    args = args.map(elem => {
-        if(elem === undefined) {
-            return 'undefined'
-        } else if(typeof elem == 'object') {
-            return JSON.stringify(elem)
-        } else {
-            return elem
-        }
-    })
-    output += args.join(" ") + "\n"
-    self.postMessage({ queryMethodArguments: [output, false] })
-  }
+    console.log = function(...args) {
+        args = args.map(elem => {
+            if(elem === undefined) {
+                return 'undefined'
+            } else if(typeof elem == 'object') {
+                return JSON.stringify(elem)
+            } else {
+                return elem
+            }
+        })
+        output += args.join(" ") + "\n"
+        self.postMessage({ queryMethodArguments: [output, false] })
+    }
 
-  globalThis.print = function(arg) {
-    console.log(JSON.stringify(arg, null, 2))
-  }
+    globalThis.print = function(arg) {
+        console.log(JSON.stringify(arg, null, 2))
+    }
 
-  try {
-    await eval(code)
-  } catch(err) {
-    console.log(err.toString())
-  }
+    try {
+        await eval(code)
+    } catch(err) {
+        console.log(err.toString())
+    }
 
-  self.postMessage({ queryMethodArguments: [output, true] })
+    self.postMessage({ queryMethodArguments: [output, true] })
 }
