43beb11aeda401d722042a53a2d6280bc253424c
Alex Schwartz <alexschwartz01@gmail.com>
2024-04-30 15:11:52 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,14 +3,25 @@ self.onmessage = async (event) => {
 
     console.log = function(...args) {
         args = args.map(elem => {
-            switch(typeof elem) {
-                case 'undefined':
-                    return 'undefined'
-                case 'object':
-                    return JSON.stringify(elem)
-                default:
-                    return elem
+            if(typeof elem == 'undefined') {
+                return 'undefined'
             }
+            
+            if(typeof elem != 'object' || elem == null) {
+                return elem
+            }
+            
+            return JSON.stringify(elem, (key, value) => {
+                if(typeof value != 'object' || value == null) {
+                    return value
+                }
+                
+                if(value.constructor.name != 'Object') {
+                    value.constructor = value.constructor.name
+                }
+                
+                return value
+            })
         })
         output += args.join(" ") + "\n"
         self.postMessage({ output })
